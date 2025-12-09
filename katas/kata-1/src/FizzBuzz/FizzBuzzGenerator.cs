27bd586817c671c32403c4c8888a4c24cbb377ba
fabian novoa <fabian93novoa@gmail.com>
2025-12-09 12:00:27 -0500

    🔵 Independencia de metodos

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -4,20 +4,25 @@ namespace FizzBuzz
     {
         public static List<string> Generate(int count)
         {
-            List<string> generate = [];
+            List<string> _generate = [];
+
             for (int initialCount = 1; initialCount <= count; initialCount++)
             {
-                if( initialCount % 3 == 0 && initialCount % 5 == 0)
-                    generate.Add("FizzBuzz");
-                else if ( initialCount % 3 == 0 )
-                    generate.Add("Fizz");
-                else if( initialCount % 5 == 0)
-                    generate.Add("Buzz");
-                else 
-                    generate.Add($"{initialCount}");
+                if (IsDivisibleForThree(initialCount) && IsDivisibleForFive(initialCount))
+                    _generate.Add("FizzBuzz");
+                else if (IsDivisibleForThree(initialCount))
+                    _generate.Add("Fizz");
+                else if (IsDivisibleForFive(initialCount))
+                    _generate.Add("Buzz");
+                else
+                    _generate.Add($"{initialCount}");
             }
 
-            return generate;
+            return _generate;
         }
+
+        private static bool IsDivisibleForFive(int initialCount) => initialCount % 5 == 0;
+
+        private static bool IsDivisibleForThree(int initialCount) => initialCount % 3 == 0;
     }
 }

f57a19e85a38708610490a4b74a1e1fcb3cba7b8
fabian novoa <fabian93novoa@gmail.com>
2025-12-09 11:04:02 -0500

    Generate_WithOne_ReturnsListWithOne

--- katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
+++ katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -16,15 +16,15 @@ namespace FizzBuzz.Tests
             result[0].Should().Be("1");
         }
         
-        [Fact]
-        public void Generate_WithFifteen_ReturnsListWithFizzBuzz()
-        {
-            // Arrange & Act
-            var result = FizzBuzzGenerator.Generate(15);
-            
-            // Assert
-            result.Should().HaveCount(15);
-            result[14].Should().Be("FizzBuzz");
-        }
+        // [Fact]
+        // public void Generate_WithFifteen_ReturnsListWithFizzBuzz()
+        // {
+        //     // Arrange & Act
+        //     var result = FizzBuzzGenerator.Generate(15);
+        //     
+        //     // Assert
+        //     result.Should().HaveCount(15);
+        //     result[14].Should().Be("FizzBuzz");
+        // }
     }
 }

a70ad9f001222b36378ad7ef154754be4e99d181
fabian novoa <fabian93novoa@gmail.com>
2025-12-09 11:38:39 -0500

    🐸Generate_WithFifteen_ReturnsListWithFizzBuzz

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -7,7 +7,9 @@ namespace FizzBuzz
             List<string> generate = [];
             for (int initialCount = 1; initialCount <= count; initialCount++)
             {
-                if ( initialCount % 3 == 0 )
+                if( initialCount % 3 == 0 && initialCount % 5 == 0)
+                    generate.Add("FizzBuzz");
+                else if ( initialCount % 3 == 0 )
                     generate.Add("Fizz");
                 else if( initialCount % 5 == 0)
                     generate.Add("Buzz");
